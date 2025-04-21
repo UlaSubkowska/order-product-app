6d44a666aad02a114731c9efc938dac627882390
UlaSubkowska <urszulasubkowska@gmail.com>
2025-04-21 21:35:23 +0200

    feat: business logic fixes

--- order/src/main/java/com/ulsub/order/OrderController.java
+++ order/src/main/java/com/ulsub/order/OrderController.java
@@ -8,8 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RestController
 @AllArgsConstructor
+@RequestMapping(value = "/v1", produces = APPLICATION_JSON_VALUE)
 public class OrderController {
 
     private final OrderService orderService;
--- order/src/main/java/com/ulsub/order/OrderMapper.java
+++ order/src/main/java/com/ulsub/order/OrderMapper.java
@@ -2,6 +2,8 @@ package com.ulsub.order;
 
 import com.ulsub.order.dto.PurchaseOrderDto;
 import com.ulsub.order.entity.PurchaseOrder;
+import com.ulsub.order.entity.PurchaseOrderItem;
+import java.math.BigDecimal;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -13,9 +15,13 @@ public abstract class OrderMapper {
 
     public PurchaseOrder mapOrderDtoToEntity(PurchaseOrderDto purchaseOrderDto) {
         PurchaseOrder purchaseOrder = mapToPurchaseOrder(purchaseOrderDto);
-        purchaseOrder
-                .getPurchaseOrderItems()
-                .forEach(purchaseOrderItem -> purchaseOrderItem.setPurchaseOrder(purchaseOrder));
+        purchaseOrder.getPurchaseOrderItems().forEach(purchaseOrderItem -> {
+            purchaseOrderItem.setPurchaseOrder(purchaseOrder);
+            purchaseOrderItem.setTotalPrice(purchaseOrderItem.getPriceItem().multiply(purchaseOrderItem.getQuantity()));
+        });
+        purchaseOrder.setTotalAmount(purchaseOrder.getPurchaseOrderItems().stream()
+                .map(PurchaseOrderItem::getTotalPrice)
+                .reduce(BigDecimal.valueOf(0), BigDecimal::add));
         return purchaseOrder;
     }
 }
